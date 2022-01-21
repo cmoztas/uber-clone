13818963ab6c6fca50c6d4a4156325d0cd811a51
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2022-01-21 16:28:59 +0300

    todoapp initial

---
+++ slices/navSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null
+}
+
+export const navSlice = createSlice
