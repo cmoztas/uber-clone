59a2179e50d8d1ff6040d7e724ab44d094b4f6e7
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2022-01-24 00:00:32 +0300

    Ride options completed

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,8 +1,8 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { withTheme } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
 import { GOOGLE_MAPS_API_KEY } from '@env';
 import { setDestination } from '../slices/navSlice'
 import { useDispatch } from 'react-redux';
@@ -42,6 +42,19 @@ const NavigateCard = () => {
                     />
                 </View>
                 <NavFavourites />
+            </View>
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                <TouchableOpacity 
+                    onPress={() => navigation.navigate('RideOptionsCard')}
+                    style={tw`flex flex-row items-center justify-between bg-black w-24 px-4 py-3 rounded-full`}
+                >
+                    <Icon name='car' type='font-awesome' color='white' size={16} />
+                    <Text style={tw`text-white text-center`}>Rides</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={tw`flex flex-row items-center justify-between w-24 px-4 py-3 rounded-full`}>
+                    <Icon name='fast-food-outline' type='ionicon' color='black' size={16} />
+                    <Text style={tw`text-center`}>Eats</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     );
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import tw from 'twrnc';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectTravelTimeInformation } from '../slices/navSlice';
+
+const data = [
+  {
+    id: 'Uber-X-123',
+    title: 'UberX',
+    multiplier: 1,
+    image: 'https://links.papareact.com/3pn'
+  },
+  {
+    id: 'Uber-X-456',
+    title: 'Uber XL',
+    multiplier: 1.2,
+    image: 'https://links.papareact.com/5w8'
+  },
+  {
+    id: 'Uber-X-789',
+    title: 'Uber LUX',
+    multiplier: 1.75,
+    image: 'https://links.papareact.com/7pf'
+  }
+]
+
+// If we have SURGE pricing, this goes up
+const SURGE_CHARGE_RATE = 1.5;
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
+
   return (
-    <View>
-      <Text>Pick a ride!</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('NavigateCard')}
+          style={tw`absolute top-3 left-5 p-3 z-1 rounded-full`}
+        >
+          <Icon name='chevron-left' type='fontawesome' />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a ride - {travelTimeInformation?.distance.text}</Text>
+      </View>
+      <FlatList 
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({item : {id, title, multiplier, image}, item}) => (
+          <TouchableOpacity 
+            style={tw`flex-row items-center justify-between px-10 ${
+              id === selected?.id && 'bg-gray-200'
+            }`}
+            onPress={() => setSelected(item)}
+          >
+            <Image 
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: 'contain'
+              }}
+              source={{uri: image}}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-bold`}>{title}</Text>
+              <Text>{travelTimeInformation?.duration.text} travel time</Text>
+            </View>
+            <Text style={tw`text-xl`}>
+              € { (travelTimeInformation?.duration.value * SURGE_CHARGE_RATE * multiplier / 100).toFixed(2) }
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View style={tw`mt-auto border-t border-gray-200`}>
+        <TouchableOpacity style={tw`bg-black py-3 m-3 ${!selected && 'bg-gray-300'}`}>
+          <Text style={tw`text-white text-center text-xl`}>Choose {selected?.title}</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
